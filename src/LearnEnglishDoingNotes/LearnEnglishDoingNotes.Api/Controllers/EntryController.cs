87ab41b39d17b0296da6bbff02f27da89d85f702
Michael Santos <unconfigured@null.spigotmc.org>
2021-07-04 21:17:38 -0300

    Add project template

---
+++ src/LearnEnglishDoingNotes/LearnEnglishDoingNotes.Api/Controllers/EntryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LearnEnglishDoingNotes.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntryController : ControllerBase
+    {
+        // GET: api/<EntryController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return null;
+        }
+    }
+}
